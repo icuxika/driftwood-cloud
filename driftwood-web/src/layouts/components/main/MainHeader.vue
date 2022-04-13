5c62614712c264fdd74c89b02047027c0fc74f94
icuxika <icuxika@outlook.com>
2022-04-13 21:25:20 +0800

    Some optimizations

--- driftwood-web/src/layouts/components/main/MainHeader.vue
+++ driftwood-web/src/layouts/components/main/MainHeader.vue
@@ -1,12 +1,17 @@
 <template>
   <n-space>
     <Draggable
-        :list="tabList" item-key="id"
-        @start="dragging = true"
-        @end="dragging =false"
         class="draggable-container"
         tag="transition-group"
-        :component-data="{tag: 'div', name: 'flip-list', type: 'transition'}"
+        :component-data="{
+          tag: 'div',
+          name: !dragging ? 'flip-list' : null,
+        }"
+        v-model="tabList"
+        v-bind="dragOptions"
+        item-key="id"
+        @start="dragging = true"
+        @end="dragging = false"
     >
       <template #item="{element}">
         <div
@@ -14,9 +19,9 @@
             :class="{'draggable-container-item-active': element.id === 1}"
             @click.stop="goto(element)"
         >
-          <span>{{element.title}}</span>
+          <span>{{ element.title }}</span>
           <n-icon size="16" v-if="!element.fixed" @click.stop="closeTab(element)">
-            <CloseIcon />
+            <CloseIcon/>
           </n-icon>
         </div>
       </template>
@@ -34,6 +39,7 @@ interface TabItem {
   title: string;
   fixed: boolean;
 }
+
 const tabList = ref<TabItem[]>([
 	{
 		id: 1,
@@ -42,19 +48,47 @@ const tabList = ref<TabItem[]>([
 	},
 	{
 		id: 2,
-		title: "用户管理",
+		title: "客户端管理",
 		fixed: false
 	},
 	{
 		id: 3,
+		title: "用户管理",
+		fixed: false
+	},
+	{
+		id: 4,
 		title: "角色管理",
 		fixed: false
 	},
+	{
+		id: 5,
+		title: "权限管理",
+		fixed: false
+	},
+	{
+		id: 6,
+		title: "菜单管理",
+		fixed: false
+	}
 ]);
+
+// 传递给vue.draggable.next(https://github.com/SortableJS/vue.draggable.next)依赖的Sortable(https://github.com/SortableJS/Sortable)的参数
+const dragOptions = {
+	group: "description",
+	animation: 250,
+	disabled: false,
+	ghostClass: "ghost"
+};
+
+// 是否处于拖拽，此判断影响是否给<transition-group>指定name属性，目前来自于vue.draggable.next的示例(https://github.com/SortableJS/vue.draggable.next/blob/master/example/components/transition-example-2.vue)
+// 实际测试，不使用列表的移动过渡(.flip-list-move),底层的Sortable似乎已经提供应有的效果了，待后期测试
 const dragging = ref(false);
 const goto = (item: TabItem) => {
 	console.log("goto: ", JSON.stringify(item));
 };
+
+// 关闭标签
 const closeTab = (item: TabItem) => {
 	console.log(item);
 };
@@ -87,10 +121,10 @@ const closeTab = (item: TabItem) => {
 }
 
 .flip-list-move {
-  transition: transform 0.25s;
+  transition: transform 0.2s ease;
 }
 
-.no-move {
-  transition: transform 0s;
+.ghost {
+  opacity: 0.5;
 }
 </style>
