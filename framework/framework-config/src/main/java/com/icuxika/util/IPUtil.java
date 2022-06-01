4e752fa3865ce8a6937c4f5a78ffb355c7f83115
icuxika <icuxika@outlook.com>
2022-06-01 17:14:17 +0800

    Add ip util to get real client ip

---
+++ framework/framework-config/src/main/java/com/icuxika/util/IPUtil.java
@@ -0,0 +1,23 @@
+package com.icuxika.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class IPUtil {
+
+    /**
+     * 获取IP地址
+     *
+     * @param request HttpServletRequest
+     * @return ip
+     */
+    public static String getIP(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if (ip != null && ip.length() > 0 && !"unknown".equalsIgnoreCase(ip)) {
+            ip = ip.split(",")[0];
+        }
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return "0:0:0:0:0:0:0:1".equals(ip) ? "127.0.0.1" : ip;
+    }
+}
