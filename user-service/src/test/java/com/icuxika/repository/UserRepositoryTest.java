0ce3196ca111e1dc8734e693eeea1b56e22a8f40
icuxika <icuxika@outlook.com>
2022-06-02 10:33:27 +0800

    Add the unit test of the paging query of user data

--- user-service/src/test/java/com/icuxika/repository/UserRepositoryTest.java
+++ user-service/src/test/java/com/icuxika/repository/UserRepositoryTest.java
@@ -1,20 +1,46 @@
 package com.icuxika.repository;
 
+import com.blazebit.persistence.CriteriaBuilderFactory;
+import com.blazebit.persistence.querydsl.BlazeJPAQuery;
 import com.icuxika.constant.SystemConstant;
+import com.icuxika.modules.user.entity.QUser;
+import com.icuxika.modules.user.entity.QUserProfile;
 import com.icuxika.modules.user.entity.User;
+import com.icuxika.modules.user.entity.UserProfile;
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.PathBuilderFactory;
+import com.querydsl.jpa.JPAExpressions;
+import com.querydsl.jpa.JPQLQuery;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.support.Querydsl;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.persistence.EntityManager;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
-@SpringBootTest
+@SpringBootTest()
 class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Autowired
+    private CriteriaBuilderFactory criteriaBuilderFactory;
 
     @Test
     void save() {
@@ -37,4 +63,52 @@ class UserRepositoryTest {
         userRepository.save(user);
     }
 
+    @Test
+    void pageByJoin() {
+        QUser qUser = QUser.user;
+        QUserProfile qUserProfile = QUserProfile.userProfile;
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        booleanBuilder.and(qUser.username.eq("icuxika"));
+        JPQLQuery<Tuple> jpqlQuery = new BlazeJPAQuery<>(entityManager, criteriaBuilderFactory)
+                .select(qUser, qUserProfile)
+                .from(qUser)
+                .leftJoin(qUserProfile)
+                .on(qUser.id.eq(qUserProfile.userId))
+                .where(booleanBuilder);
+        long fetchCount = jpqlQuery.fetchCount();
+        Pageable pageable = PageRequest.of(0, 5, Sort.by("id").descending());
+        Querydsl querydsl = new Querydsl(entityManager, new PathBuilderFactory().create(User.class));
+        jpqlQuery = querydsl.applyPagination(pageable, jpqlQuery);
+        List<Tuple> list = jpqlQuery.fetch();
+
+        List<User> userList = new ArrayList<>();
+        List<UserProfile> userProfileList = new ArrayList<>();
+        list.forEach(tuple -> {
+            Optional.ofNullable(tuple.get(qUser)).ifPresent(userList::add);
+            Optional.ofNullable(tuple.get(qUserProfile)).ifPresent(userProfileList::add);
+        });
+        Assertions.assertEquals(1, fetchCount);
+        Assertions.assertEquals(1, userList.size());
+        Assertions.assertEquals(1, userProfileList.size());
+    }
+
+    @Test
+    void pageByExists() {
+        QUser qUser = QUser.user;
+        QUserProfile qUserProfile = QUserProfile.userProfile;
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        booleanBuilder.and(qUser.username.eq("icuxika"));
+        booleanBuilder.and(
+                JPAExpressions
+                        .select(qUserProfile)
+                        .from(qUserProfile)
+                        .where(qUser.id.eq(qUserProfile.userId))
+                        .exists()
+        );
+        Pageable pageable = PageRequest.of(0, 5, Sort.by("id").descending());
+        Page<User> userPage = userRepository.findAll(booleanBuilder, pageable);
+        Assertions.assertEquals(1, userPage.getTotalElements());
+    }
 }
