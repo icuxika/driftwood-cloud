75956146c89457c49bc4ac22099062d0ca25d214
icuxika <icuxika@outlook.com>
2022-08-19 16:50:14 +0800

    Word generator

---
+++ framework-extension/framework-extension-office/src/test/java/com/icuxika/util/WordTemplateUtilTest.java
@@ -0,0 +1,19 @@
+package com.icuxika.util;
+
+import freemarker.template.Template;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+class WordTemplateUtilTest {
+
+    public static void main(String[] args) {
+        Template template = WordTemplateUtil.getTemplate("C:\\Users\\icuxika\\IdeaProjects\\driftwood-cloud\\framework-extension\\framework-extension-office\\src\\main\\resources\\template", "测试模板.xml");
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "名称");
+        map.put("desc", "描述");
+        File xml = WordTemplateUtil.overrideXML(template, map, "C:\\Users\\icuxika\\Desktop\\out.xml");
+        WordTemplateUtil.overrideDOCX(xml, "C:\\Users\\icuxika\\IdeaProjects\\driftwood-cloud\\framework-extension\\framework-extension-office\\src\\main\\resources\\template\\测试模板.docx", "C:\\Users\\icuxika\\Desktop\\out.docx");
+    }
+}
