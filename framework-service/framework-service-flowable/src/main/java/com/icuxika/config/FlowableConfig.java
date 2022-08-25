b265c164e5c2c0d8c9bc87fcefa4da16c6c5cd60
icuxika <icuxika@outlook.com>
2022-08-25 17:12:02 +0800

    Generate diagram of process

---
+++ framework-service/framework-service-flowable/src/main/java/com/icuxika/config/FlowableConfig.java
@@ -0,0 +1,17 @@
+package com.icuxika.config;
+
+import org.flowable.spring.SpringProcessEngineConfiguration;
+import org.flowable.spring.boot.EngineConfigurationConfigurer;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlowableConfig implements EngineConfigurationConfigurer<SpringProcessEngineConfiguration> {
+
+    @Override
+    public void configure(SpringProcessEngineConfiguration engineConfiguration) {
+        engineConfiguration.setActivityFontName("宋体");
+        engineConfiguration.setLabelFontName("宋体");
+        engineConfiguration.setAnnotationFontName("宋体");
+    }
+
+}
--- framework-service/framework-service-flowable/src/main/java/com/icuxika/controller/ProcessController.java
+++ framework-service/framework-service-flowable/src/main/java/com/icuxika/controller/ProcessController.java
@@ -5,13 +5,17 @@ import com.icuxika.transfer.flowable.NewProcessDTO;
 import com.icuxika.transfer.flowable.ProcessTaskDTO;
 import com.icuxika.transfer.flowable.TaskVO;
 import com.icuxika.util.SecurityUtil;
+import org.flowable.bpmn.model.BpmnModel;
 import org.flowable.common.engine.impl.identity.Authentication;
-import org.flowable.engine.RepositoryService;
-import org.flowable.engine.RuntimeService;
-import org.flowable.engine.TaskService;
+import org.flowable.engine.*;
+import org.flowable.engine.history.HistoricActivityInstance;
+import org.flowable.engine.history.HistoricProcessInstance;
 import org.flowable.engine.repository.ProcessDefinition;
 import org.flowable.engine.runtime.ProcessInstance;
+import org.flowable.image.ProcessDiagramGenerator;
+import org.flowable.image.impl.DefaultProcessDiagramGenerator;
 import org.flowable.task.api.Task;
+import org.flowable.task.api.history.HistoricTaskInstance;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -20,6 +24,12 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -34,6 +44,12 @@ public class ProcessController {
 
     @Autowired
     private RepositoryService repositoryService;
+
+    @Autowired
+    private ProcessEngine processEngine;
+
+    @Autowired
+    private HistoryService historyService;
 
     /**
      * 创建流程
@@ -73,7 +89,7 @@ public class ProcessController {
      *
      * @return
      */
-    @PreAuthorize("@fvs.isFeign(#request) || hasRole('USER')")
+    @PreAuthorize("@fvs.isFeign(#request) || hasRole('ADMIN')")
     @GetMapping("getTaskPage")
     public ApiData<List<TaskVO>> getTaskPage(@PageableDefault Pageable pageable, String userId, HttpServletRequest request) {
         List<Task> taskList = taskService.createTaskQuery().taskCandidateOrAssigned(userId).listPage((int) pageable.getOffset(), pageable.getPageSize());
@@ -85,10 +101,16 @@ public class ProcessController {
      *
      * @return
      */
-    @PreAuthorize("@fvs.isFeign(#request) || hasRole('USER')")
+    @PreAuthorize("@fvs.isFeign(#request) || hasRole('ADMIN')")
     @GetMapping("getTaskList")
     public ApiData<List<TaskVO>> getTaskList(HttpServletRequest request) {
         return ApiData.ok(task2VO(taskService.createTaskQuery().list()));
+    }
+
+    @PreAuthorize("@fvs.isFeign(#request) || hasRole('ADMIN')")
+    @GetMapping("getHistoricTaskInstance")
+    public ApiData<List<HistoricTaskInstance>> getHistoricTaskInstance(HttpServletRequest request) {
+        return ApiData.ok(historyService.createHistoricTaskInstanceQuery().list());
     }
 
     /**
@@ -100,6 +122,70 @@ public class ProcessController {
     @GetMapping("getProcessDefinitionList")
     public ApiData<List<ProcessDefinition>> getProcessDefinitionList(HttpServletRequest request) {
         return ApiData.ok(repositoryService.createProcessDefinitionQuery().list());
+    }
+
+    /**
+     * 获取流程图
+     */
+    @PreAuthorize("@fvs.isFeign(#request) || hasRole('ADMIN')")
+    @GetMapping("getProcessDiagram")
+    public void getProcessDiagram(@RequestParam String processDefinitionId, HttpServletRequest request, HttpServletResponse response) {
+        BpmnModel bpmnModel = repositoryService.getBpmnModel(processDefinitionId);
+        ProcessEngineConfiguration engineConfiguration = processEngine.getProcessEngineConfiguration();
+        ProcessDiagramGenerator diagramGenerator = new DefaultProcessDiagramGenerator();
+        try (
+                InputStream inputStream = diagramGenerator.generateDiagram(bpmnModel, "png", Collections.EMPTY_LIST, Collections.EMPTY_LIST, engineConfiguration.getActivityFontName(), engineConfiguration.getLabelFontName(), engineConfiguration.getAnnotationFontName(), engineConfiguration.getClassLoader(), 1.0, true);
+                OutputStream outputStream = response.getOutputStream();
+        ) {
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, length);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    /**
+     * 获取任务trace流程图
+     */
+    @PreAuthorize("@fvs.isFeign(#request) || hasRole('ADMIN')")
+    @GetMapping("getProcessDiagramTrace")
+    public void getProcessDiagramTrace(@RequestParam String processInstanceId, HttpServletRequest request, HttpServletResponse response) {
+        String processDefinitionId;
+        // TODO 流程是否已经结束判断
+        HistoricProcessInstance historicProcessInstance = historyService.createHistoricProcessInstanceQuery().processInstanceId(processInstanceId).singleResult();
+        processDefinitionId = historicProcessInstance.getProcessDefinitionId();
+
+        List<HistoricActivityInstance> historicActivityInstanceList = historyService.createHistoricActivityInstanceQuery().processInstanceId(processInstanceId).orderByHistoricActivityInstanceStartTime().asc().list();
+        List<String> highLightedActivities = new ArrayList<>();
+        List<String> highLightedFlows = new ArrayList<>();
+        historicActivityInstanceList.forEach(historicActivityInstance -> {
+            String activityId = historicActivityInstance.getActivityId();
+            highLightedActivities.add(activityId);
+            if ("sequenceFlow".equals(historicActivityInstance.getActivityType())) {
+                highLightedFlows.add(activityId);
+            }
+        });
+
+        BpmnModel bpmnModel = repositoryService.getBpmnModel(processDefinitionId);
+        ProcessEngineConfiguration engineConfiguration = processEngine.getProcessEngineConfiguration();
+        ProcessDiagramGenerator diagramGenerator = engineConfiguration.getProcessDiagramGenerator();
+
+        try (
+                InputStream inputStream = diagramGenerator.generateDiagram(bpmnModel, "png", highLightedActivities, highLightedFlows, engineConfiguration.getActivityFontName(), engineConfiguration.getLabelFontName(), engineConfiguration.getAnnotationFontName(), engineConfiguration.getClassLoader(), 1.0, true);
+                OutputStream outputStream = response.getOutputStream();
+        ) {
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, length);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     private List<TaskVO> task2VO(List<Task> taskList) {
