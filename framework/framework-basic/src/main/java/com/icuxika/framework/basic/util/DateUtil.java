926d4787bd84335c9f56703377182297ed1df9f0
icuxika <icuxika@outlook.com>
2022-11-28 13:45:07 +0800

    Some optimizations

--- framework/framework-basic/src/main/java/com/icuxika/framework/basic/util/DateUtil.java
+++ framework/framework-basic/src/main/java/com/icuxika/framework/basic/util/DateUtil.java
@@ -1,6 +1,8 @@
 package com.icuxika.framework.basic.util;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateUtil {
@@ -11,5 +13,21 @@ public class DateUtil {
 
     public static String getLocalDateTimeText(LocalDateTime localDateTime) {
         return localDateTime.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+    }
+
+    public static String getLocalDateText() {
+        return getLocalDateText(LocalDate.now());
+    }
+
+    public static String getLocalDateText(LocalDate localDate) {
+        return localDate.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+    }
+
+    public static String getLocalTimeText() {
+        return getLocalTimeText(LocalTime.now());
+    }
+
+    public static String getLocalTimeText(LocalTime localTime) {
+        return localTime.format(DateTimeFormatter.ofPattern("HHmmss"));
     }
 }
