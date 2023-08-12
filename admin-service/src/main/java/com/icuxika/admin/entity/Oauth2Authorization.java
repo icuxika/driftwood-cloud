bb51a9bbde5ae3dc7e9fc55c2729ba0087660872
icuxika <icuxika@outlook.com>
2023-08-12 23:12:31 +0800

    Dependency Upgrades     * Update to Spring Boot 3.1.2     * Update to Spring Cloud 2022.0.3     * Update to Spring Cloud Alibaba 2022.0.0.0     * Update to Spring Authorization Server 1.1.1

--- admin-service/src/main/java/com/icuxika/admin/entity/Oauth2Authorization.java
+++ admin-service/src/main/java/com/icuxika/admin/entity/Oauth2Authorization.java
@@ -1,15 +1,13 @@
 package com.icuxika.admin.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "oauth2_authorization")
 public class Oauth2Authorization {
+
     @Id
     @Column(name = "id", nullable = false, length = 100)
     private String id;
@@ -23,240 +21,112 @@ public class Oauth2Authorization {
     @Column(name = "authorization_grant_type", nullable = false, length = 100)
     private String authorizationGrantType;
 
-    @Column(name = "attributes")
+    @Column(name = "authorized_scopes", nullable = true, length = 1000)
+    private String authorizedScopes;
+
+    @Lob
+    @Column(name = "attributes", nullable = true, columnDefinition = "BLOB")
     private byte[] attributes;
 
-    @Column(name = "state", length = 500)
+    @Column(name = "state", nullable = true, length = 500)
     private String state;
 
-    @Column(name = "authorization_code_value")
+    @Lob
+    @Column(name = "authorization_code_value", nullable = true, columnDefinition = "BLOB")
     private byte[] authorizationCodeValue;
 
-    @Column(name = "authorization_code_issued_at")
+    @Column(name = "authorization_code_issued_at", nullable = true)
     private LocalDateTime authorizationCodeIssuedAt;
 
-    @Column(name = "authorization_code_expires_at")
+    @Column(name = "authorization_code_expires_at", nullable = true)
     private LocalDateTime authorizationCodeExpiresAt;
 
-    @Column(name = "authorization_code_metadata")
+    @Lob
+    @Column(name = "authorization_code_metadata", nullable = true, columnDefinition = "BLOB")
     private byte[] authorizationCodeMetadata;
 
-    @Column(name = "access_token_value")
+    @Lob
+    @Column(name = "access_token_value", nullable = true, columnDefinition = "BLOB")
     private byte[] accessTokenValue;
 
-    @Column(name = "access_token_issued_at")
+    @Column(name = "access_token_issued_at", nullable = true)
     private LocalDateTime accessTokenIssuedAt;
 
-    @Column(name = "access_token_expires_at")
+    @Column(name = "access_token_expires_at", nullable = true)
     private LocalDateTime accessTokenExpiresAt;
 
-    @Column(name = "access_token_metadata")
+    @Lob
+    @Column(name = "access_token_metadata", nullable = true, columnDefinition = "BLOB")
     private byte[] accessTokenMetadata;
 
-    @Column(name = "access_token_type", length = 100)
+    @Column(name = "access_token_type", nullable = true, length = 100)
     private String accessTokenType;
 
-    @Column(name = "access_token_scopes", length = 1000)
+    @Column(name = "access_token_scopes", nullable = true, length = 1000)
     private String accessTokenScopes;
 
-    @Column(name = "oidc_id_token_value")
+    @Lob
+    @Column(name = "oidc_id_token_value", nullable = true, columnDefinition = "BLOB")
     private byte[] oidcIdTokenValue;
 
-    @Column(name = "oidc_id_token_issued_at")
+    @Column(name = "oidc_id_token_issued_at", nullable = true)
     private LocalDateTime oidcIdTokenIssuedAt;
 
-    @Column(name = "oidc_id_token_expires_at")
+    @Column(name = "oidc_id_token_expires_at", nullable = true)
     private LocalDateTime oidcIdTokenExpiresAt;
 
-    @Column(name = "oidc_id_token_metadata")
+    @Lob
+    @Column(name = "oidc_id_token_metadata", nullable = true, columnDefinition = "BLOB")
     private byte[] oidcIdTokenMetadata;
 
-    @Column(name = "refresh_token_value")
+    @Lob
+    @Column(name = "refresh_token_value", nullable = true, columnDefinition = "BLOB")
     private byte[] refreshTokenValue;
 
-    @Column(name = "refresh_token_issued_at")
+    @Column(name = "refresh_token_issued_at", nullable = true)
     private LocalDateTime refreshTokenIssuedAt;
 
-    @Column(name = "refresh_token_expires_at")
+    @Column(name = "refresh_token_expires_at", nullable = true)
     private LocalDateTime refreshTokenExpiresAt;
 
-    @Column(name = "refresh_token_metadata")
+    @Lob
+    @Column(name = "refresh_token_metadata", nullable = true, columnDefinition = "BLOB")
     private byte[] refreshTokenMetadata;
 
-    public byte[] getRefreshTokenMetadata() {
-        return refreshTokenMetadata;
+    @Lob
+    @Column(name = "user_code_value", nullable = true, columnDefinition = "BLOB")
+    private byte[] userCodeValue;
+
+    @Column(name = "user_code_issued_at", nullable = true)
+    private LocalDateTime userCodeIssuedAt;
+
+    @Column(name = "user_code_expires_at", nullable = true)
+    private LocalDateTime userCodeExpiresAt;
+
+    @Lob
+    @Column(name = "user_code_metadata", nullable = true, columnDefinition = "BLOB")
+    private byte[] userCodeMetadata;
+
+    @Lob
+    @Column(name = "device_code_value", nullable = true, columnDefinition = "BLOB")
+    private byte[] deviceCodeValue;
+
+    @Column(name = "device_code_issued_at", nullable = true)
+    private LocalDateTime deviceCodeIssuedAt;
+
+    @Column(name = "device_code_expires_at", nullable = true)
+    private LocalDateTime deviceCodeExpiresAt;
+
+    @Lob
+    @Column(name = "device_code_metadata", nullable = true, columnDefinition = "BLOB")
+    private byte[] deviceCodeMetadata;
+
+    public String getId() {
+        return id;
     }
 
-    public void setRefreshTokenMetadata(byte[] refreshTokenMetadata) {
-        this.refreshTokenMetadata = refreshTokenMetadata;
-    }
-
-    public LocalDateTime getRefreshTokenExpiresAt() {
-        return refreshTokenExpiresAt;
-    }
-
-    public void setRefreshTokenExpiresAt(LocalDateTime refreshTokenExpiresAt) {
-        this.refreshTokenExpiresAt = refreshTokenExpiresAt;
-    }
-
-    public LocalDateTime getRefreshTokenIssuedAt() {
-        return refreshTokenIssuedAt;
-    }
-
-    public void setRefreshTokenIssuedAt(LocalDateTime refreshTokenIssuedAt) {
-        this.refreshTokenIssuedAt = refreshTokenIssuedAt;
-    }
-
-    public byte[] getRefreshTokenValue() {
-        return refreshTokenValue;
-    }
-
-    public void setRefreshTokenValue(byte[] refreshTokenValue) {
-        this.refreshTokenValue = refreshTokenValue;
-    }
-
-    public byte[] getOidcIdTokenMetadata() {
-        return oidcIdTokenMetadata;
-    }
-
-    public void setOidcIdTokenMetadata(byte[] oidcIdTokenMetadata) {
-        this.oidcIdTokenMetadata = oidcIdTokenMetadata;
-    }
-
-    public LocalDateTime getOidcIdTokenExpiresAt() {
-        return oidcIdTokenExpiresAt;
-    }
-
-    public void setOidcIdTokenExpiresAt(LocalDateTime oidcIdTokenExpiresAt) {
-        this.oidcIdTokenExpiresAt = oidcIdTokenExpiresAt;
-    }
-
-    public LocalDateTime getOidcIdTokenIssuedAt() {
-        return oidcIdTokenIssuedAt;
-    }
-
-    public void setOidcIdTokenIssuedAt(LocalDateTime oidcIdTokenIssuedAt) {
-        this.oidcIdTokenIssuedAt = oidcIdTokenIssuedAt;
-    }
-
-    public byte[] getOidcIdTokenValue() {
-        return oidcIdTokenValue;
-    }
-
-    public void setOidcIdTokenValue(byte[] oidcIdTokenValue) {
-        this.oidcIdTokenValue = oidcIdTokenValue;
-    }
-
-    public String getAccessTokenScopes() {
-        return accessTokenScopes;
-    }
-
-    public void setAccessTokenScopes(String accessTokenScopes) {
-        this.accessTokenScopes = accessTokenScopes;
-    }
-
-    public String getAccessTokenType() {
-        return accessTokenType;
-    }
-
-    public void setAccessTokenType(String accessTokenType) {
-        this.accessTokenType = accessTokenType;
-    }
-
-    public byte[] getAccessTokenMetadata() {
-        return accessTokenMetadata;
-    }
-
-    public void setAccessTokenMetadata(byte[] accessTokenMetadata) {
-        this.accessTokenMetadata = accessTokenMetadata;
-    }
-
-    public LocalDateTime getAccessTokenExpiresAt() {
-        return accessTokenExpiresAt;
-    }
-
-    public void setAccessTokenExpiresAt(LocalDateTime accessTokenExpiresAt) {
-        this.accessTokenExpiresAt = accessTokenExpiresAt;
-    }
-
-    public LocalDateTime getAccessTokenIssuedAt() {
-        return accessTokenIssuedAt;
-    }
-
-    public void setAccessTokenIssuedAt(LocalDateTime accessTokenIssuedAt) {
-        this.accessTokenIssuedAt = accessTokenIssuedAt;
-    }
-
-    public byte[] getAccessTokenValue() {
-        return accessTokenValue;
-    }
-
-    public void setAccessTokenValue(byte[] accessTokenValue) {
-        this.accessTokenValue = accessTokenValue;
-    }
-
-    public byte[] getAuthorizationCodeMetadata() {
-        return authorizationCodeMetadata;
-    }
-
-    public void setAuthorizationCodeMetadata(byte[] authorizationCodeMetadata) {
-        this.authorizationCodeMetadata = authorizationCodeMetadata;
-    }
-
-    public LocalDateTime getAuthorizationCodeExpiresAt() {
-        return authorizationCodeExpiresAt;
-    }
-
-    public void setAuthorizationCodeExpiresAt(LocalDateTime authorizationCodeExpiresAt) {
-        this.authorizationCodeExpiresAt = authorizationCodeExpiresAt;
-    }
-
-    public LocalDateTime getAuthorizationCodeIssuedAt() {
-        return authorizationCodeIssuedAt;
-    }
-
-    public void setAuthorizationCodeIssuedAt(LocalDateTime authorizationCodeIssuedAt) {
-        this.authorizationCodeIssuedAt = authorizationCodeIssuedAt;
-    }
-
-    public byte[] getAuthorizationCodeValue() {
-        return authorizationCodeValue;
-    }
-
-    public void setAuthorizationCodeValue(byte[] authorizationCodeValue) {
-        this.authorizationCodeValue = authorizationCodeValue;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public byte[] getAttributes() {
-        return attributes;
-    }
-
-    public void setAttributes(byte[] attributes) {
-        this.attributes = attributes;
-    }
-
-    public String getAuthorizationGrantType() {
-        return authorizationGrantType;
-    }
-
-    public void setAuthorizationGrantType(String authorizationGrantType) {
-        this.authorizationGrantType = authorizationGrantType;
-    }
-
-    public String getPrincipalName() {
-        return principalName;
-    }
-
-    public void setPrincipalName(String principalName) {
-        this.principalName = principalName;
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getRegisteredClientId() {
@@ -267,11 +137,252 @@ public class Oauth2Authorization {
         this.registeredClientId = registeredClientId;
     }
 
-    public String getId() {
-        return id;
+    public String getPrincipalName() {
+        return principalName;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setPrincipalName(String principalName) {
+        this.principalName = principalName;
     }
+
+    public String getAuthorizationGrantType() {
+        return authorizationGrantType;
+    }
+
+    public void setAuthorizationGrantType(String authorizationGrantType) {
+        this.authorizationGrantType = authorizationGrantType;
+    }
+
+    public String getAuthorizedScopes() {
+        return authorizedScopes;
+    }
+
+    public void setAuthorizedScopes(String authorizedScopes) {
+        this.authorizedScopes = authorizedScopes;
+    }
+
+    public byte[] getAttributes() {
+        return attributes;
+    }
+
+    public void setAttributes(byte[] attributes) {
+        this.attributes = attributes;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public byte[] getAuthorizationCodeValue() {
+        return authorizationCodeValue;
+    }
+
+    public void setAuthorizationCodeValue(byte[] authorizationCodeValue) {
+        this.authorizationCodeValue = authorizationCodeValue;
+    }
+
+    public LocalDateTime getAuthorizationCodeIssuedAt() {
+        return authorizationCodeIssuedAt;
+    }
+
+    public void setAuthorizationCodeIssuedAt(LocalDateTime authorizationCodeIssuedAt) {
+        this.authorizationCodeIssuedAt = authorizationCodeIssuedAt;
+    }
+
+    public LocalDateTime getAuthorizationCodeExpiresAt() {
+        return authorizationCodeExpiresAt;
+    }
+
+    public void setAuthorizationCodeExpiresAt(LocalDateTime authorizationCodeExpiresAt) {
+        this.authorizationCodeExpiresAt = authorizationCodeExpiresAt;
+    }
+
+    public byte[] getAuthorizationCodeMetadata() {
+        return authorizationCodeMetadata;
+    }
+
+    public void setAuthorizationCodeMetadata(byte[] authorizationCodeMetadata) {
+        this.authorizationCodeMetadata = authorizationCodeMetadata;
+    }
+
+    public byte[] getAccessTokenValue() {
+        return accessTokenValue;
+    }
+
+    public void setAccessTokenValue(byte[] accessTokenValue) {
+        this.accessTokenValue = accessTokenValue;
+    }
+
+    public LocalDateTime getAccessTokenIssuedAt() {
+        return accessTokenIssuedAt;
+    }
+
+    public void setAccessTokenIssuedAt(LocalDateTime accessTokenIssuedAt) {
+        this.accessTokenIssuedAt = accessTokenIssuedAt;
+    }
+
+    public LocalDateTime getAccessTokenExpiresAt() {
+        return accessTokenExpiresAt;
+    }
+
+    public void setAccessTokenExpiresAt(LocalDateTime accessTokenExpiresAt) {
+        this.accessTokenExpiresAt = accessTokenExpiresAt;
+    }
+
+    public byte[] getAccessTokenMetadata() {
+        return accessTokenMetadata;
+    }
+
+    public void setAccessTokenMetadata(byte[] accessTokenMetadata) {
+        this.accessTokenMetadata = accessTokenMetadata;
+    }
+
+    public String getAccessTokenType() {
+        return accessTokenType;
+    }
+
+    public void setAccessTokenType(String accessTokenType) {
+        this.accessTokenType = accessTokenType;
+    }
+
+    public String getAccessTokenScopes() {
+        return accessTokenScopes;
+    }
+
+    public void setAccessTokenScopes(String accessTokenScopes) {
+        this.accessTokenScopes = accessTokenScopes;
+    }
+
+    public byte[] getOidcIdTokenValue() {
+        return oidcIdTokenValue;
+    }
+
+    public void setOidcIdTokenValue(byte[] oidcIdTokenValue) {
+        this.oidcIdTokenValue = oidcIdTokenValue;
+    }
+
+    public LocalDateTime getOidcIdTokenIssuedAt() {
+        return oidcIdTokenIssuedAt;
+    }
+
+    public void setOidcIdTokenIssuedAt(LocalDateTime oidcIdTokenIssuedAt) {
+        this.oidcIdTokenIssuedAt = oidcIdTokenIssuedAt;
+    }
+
+    public LocalDateTime getOidcIdTokenExpiresAt() {
+        return oidcIdTokenExpiresAt;
+    }
+
+    public void setOidcIdTokenExpiresAt(LocalDateTime oidcIdTokenExpiresAt) {
+        this.oidcIdTokenExpiresAt = oidcIdTokenExpiresAt;
+    }
+
+    public byte[] getOidcIdTokenMetadata() {
+        return oidcIdTokenMetadata;
+    }
+
+    public void setOidcIdTokenMetadata(byte[] oidcIdTokenMetadata) {
+        this.oidcIdTokenMetadata = oidcIdTokenMetadata;
+    }
+
+    public byte[] getRefreshTokenValue() {
+        return refreshTokenValue;
+    }
+
+    public void setRefreshTokenValue(byte[] refreshTokenValue) {
+        this.refreshTokenValue = refreshTokenValue;
+    }
+
+    public LocalDateTime getRefreshTokenIssuedAt() {
+        return refreshTokenIssuedAt;
+    }
+
+    public void setRefreshTokenIssuedAt(LocalDateTime refreshTokenIssuedAt) {
+        this.refreshTokenIssuedAt = refreshTokenIssuedAt;
+    }
+
+    public LocalDateTime getRefreshTokenExpiresAt() {
+        return refreshTokenExpiresAt;
+    }
+
+    public void setRefreshTokenExpiresAt(LocalDateTime refreshTokenExpiresAt) {
+        this.refreshTokenExpiresAt = refreshTokenExpiresAt;
+    }
+
+    public byte[] getRefreshTokenMetadata() {
+        return refreshTokenMetadata;
+    }
+
+    public void setRefreshTokenMetadata(byte[] refreshTokenMetadata) {
+        this.refreshTokenMetadata = refreshTokenMetadata;
+    }
+
+    public byte[] getUserCodeValue() {
+        return userCodeValue;
+    }
+
+    public void setUserCodeValue(byte[] userCodeValue) {
+        this.userCodeValue = userCodeValue;
+    }
+
+    public LocalDateTime getUserCodeIssuedAt() {
+        return userCodeIssuedAt;
+    }
+
+    public void setUserCodeIssuedAt(LocalDateTime userCodeIssuedAt) {
+        this.userCodeIssuedAt = userCodeIssuedAt;
+    }
+
+    public LocalDateTime getUserCodeExpiresAt() {
+        return userCodeExpiresAt;
+    }
+
+    public void setUserCodeExpiresAt(LocalDateTime userCodeExpiresAt) {
+        this.userCodeExpiresAt = userCodeExpiresAt;
+    }
+
+    public byte[] getUserCodeMetadata() {
+        return userCodeMetadata;
+    }
+
+    public void setUserCodeMetadata(byte[] userCodeMetadata) {
+        this.userCodeMetadata = userCodeMetadata;
+    }
+
+    public byte[] getDeviceCodeValue() {
+        return deviceCodeValue;
+    }
+
+    public void setDeviceCodeValue(byte[] deviceCodeValue) {
+        this.deviceCodeValue = deviceCodeValue;
+    }
+
+    public LocalDateTime getDeviceCodeIssuedAt() {
+        return deviceCodeIssuedAt;
+    }
+
+    public void setDeviceCodeIssuedAt(LocalDateTime deviceCodeIssuedAt) {
+        this.deviceCodeIssuedAt = deviceCodeIssuedAt;
+    }
+
+    public LocalDateTime getDeviceCodeExpiresAt() {
+        return deviceCodeExpiresAt;
+    }
+
+    public void setDeviceCodeExpiresAt(LocalDateTime deviceCodeExpiresAt) {
+        this.deviceCodeExpiresAt = deviceCodeExpiresAt;
+    }
+
+    public byte[] getDeviceCodeMetadata() {
+        return deviceCodeMetadata;
+    }
+
+    public void setDeviceCodeMetadata(byte[] deviceCodeMetadata) {
+        this.deviceCodeMetadata = deviceCodeMetadata;
+    }
+
 }
--- admin-service/src/main/java/com/icuxika/admin/entity/Oauth2RegisteredClient.java
+++ admin-service/src/main/java/com/icuxika/admin/entity/Oauth2RegisteredClient.java
@@ -10,6 +10,7 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "oauth2_registered_client")
 public class Oauth2RegisteredClient {
+
     @Id
     @Column(name = "id", nullable = false, length = 100)
     private String id;
@@ -20,10 +21,10 @@ public class Oauth2RegisteredClient {
     @Column(name = "client_id_issued_at", nullable = false)
     private LocalDateTime clientIdIssuedAt;
 
-    @Column(name = "client_secret", length = 200)
+    @Column(name = "client_secret", nullable = true, length = 200)
     private String clientSecret;
 
-    @Column(name = "client_secret_expires_at")
+    @Column(name = "client_secret_expires_at", nullable = true)
     private LocalDateTime clientSecretExpiresAt;
 
     @Column(name = "client_name", nullable = false, length = 200)
@@ -35,8 +36,11 @@ public class Oauth2RegisteredClient {
     @Column(name = "authorization_grant_types", nullable = false, length = 1000)
     private String authorizationGrantTypes;
 
-    @Column(name = "redirect_uris", length = 1000)
+    @Column(name = "redirect_uris", nullable = true, length = 1000)
     private String redirectUris;
+
+    @Column(name = "post_logout_redirect_uris", nullable = true, length = 1000)
+    private String postLogoutRedirectUris;
 
     @Column(name = "scopes", nullable = false, length = 1000)
     private String scopes;
@@ -47,84 +51,12 @@ public class Oauth2RegisteredClient {
     @Column(name = "token_settings", nullable = false, length = 2000)
     private String tokenSettings;
 
-    public String getTokenSettings() {
-        return tokenSettings;
+    public String getId() {
+        return id;
     }
 
-    public void setTokenSettings(String tokenSettings) {
-        this.tokenSettings = tokenSettings;
-    }
-
-    public String getClientSettings() {
-        return clientSettings;
-    }
-
-    public void setClientSettings(String clientSettings) {
-        this.clientSettings = clientSettings;
-    }
-
-    public String getScopes() {
-        return scopes;
-    }
-
-    public void setScopes(String scopes) {
-        this.scopes = scopes;
-    }
-
-    public String getRedirectUris() {
-        return redirectUris;
-    }
-
-    public void setRedirectUris(String redirectUris) {
-        this.redirectUris = redirectUris;
-    }
-
-    public String getAuthorizationGrantTypes() {
-        return authorizationGrantTypes;
-    }
-
-    public void setAuthorizationGrantTypes(String authorizationGrantTypes) {
-        this.authorizationGrantTypes = authorizationGrantTypes;
-    }
-
-    public String getClientAuthenticationMethods() {
-        return clientAuthenticationMethods;
-    }
-
-    public void setClientAuthenticationMethods(String clientAuthenticationMethods) {
-        this.clientAuthenticationMethods = clientAuthenticationMethods;
-    }
-
-    public String getClientName() {
-        return clientName;
-    }
-
-    public void setClientName(String clientName) {
-        this.clientName = clientName;
-    }
-
-    public LocalDateTime getClientSecretExpiresAt() {
-        return clientSecretExpiresAt;
-    }
-
-    public void setClientSecretExpiresAt(LocalDateTime clientSecretExpiresAt) {
-        this.clientSecretExpiresAt = clientSecretExpiresAt;
-    }
-
-    public String getClientSecret() {
-        return clientSecret;
-    }
-
-    public void setClientSecret(String clientSecret) {
-        this.clientSecret = clientSecret;
-    }
-
-    public LocalDateTime getClientIdIssuedAt() {
-        return clientIdIssuedAt;
-    }
-
-    public void setClientIdIssuedAt(LocalDateTime clientIdIssuedAt) {
-        this.clientIdIssuedAt = clientIdIssuedAt;
+    public void setId(String id) {
+        this.id = id;
     }
 
     public String getClientId() {
@@ -135,11 +67,92 @@ public class Oauth2RegisteredClient {
         this.clientId = clientId;
     }
 
-    public String getId() {
-        return id;
+    public LocalDateTime getClientIdIssuedAt() {
+        return clientIdIssuedAt;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setClientIdIssuedAt(LocalDateTime clientIdIssuedAt) {
+        this.clientIdIssuedAt = clientIdIssuedAt;
     }
+
+    public String getClientSecret() {
+        return clientSecret;
+    }
+
+    public void setClientSecret(String clientSecret) {
+        this.clientSecret = clientSecret;
+    }
+
+    public LocalDateTime getClientSecretExpiresAt() {
+        return clientSecretExpiresAt;
+    }
+
+    public void setClientSecretExpiresAt(LocalDateTime clientSecretExpiresAt) {
+        this.clientSecretExpiresAt = clientSecretExpiresAt;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getClientAuthenticationMethods() {
+        return clientAuthenticationMethods;
+    }
+
+    public void setClientAuthenticationMethods(String clientAuthenticationMethods) {
+        this.clientAuthenticationMethods = clientAuthenticationMethods;
+    }
+
+    public String getAuthorizationGrantTypes() {
+        return authorizationGrantTypes;
+    }
+
+    public void setAuthorizationGrantTypes(String authorizationGrantTypes) {
+        this.authorizationGrantTypes = authorizationGrantTypes;
+    }
+
+    public String getRedirectUris() {
+        return redirectUris;
+    }
+
+    public void setRedirectUris(String redirectUris) {
+        this.redirectUris = redirectUris;
+    }
+
+    public String getPostLogoutRedirectUris() {
+        return postLogoutRedirectUris;
+    }
+
+    public void setPostLogoutRedirectUris(String postLogoutRedirectUris) {
+        this.postLogoutRedirectUris = postLogoutRedirectUris;
+    }
+
+    public String getScopes() {
+        return scopes;
+    }
+
+    public void setScopes(String scopes) {
+        this.scopes = scopes;
+    }
+
+    public String getClientSettings() {
+        return clientSettings;
+    }
+
+    public void setClientSettings(String clientSettings) {
+        this.clientSettings = clientSettings;
+    }
+
+    public String getTokenSettings() {
+        return tokenSettings;
+    }
+
+    public void setTokenSettings(String tokenSettings) {
+        this.tokenSettings = tokenSettings;
+    }
+
 }
